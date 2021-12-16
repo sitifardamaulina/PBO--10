f0e84b2c37a09e371fda9e296518788985dec5d2
sitifardamaulina <80209705+sitifardamaulina@users.noreply.github.com>
2021-12-16 09:05:28 +0800

    Add files via upload

---
+++ Phone.java
@@ -0,0 +1,12 @@
+
+package Tugas10;
+
+public interface Phone {
+    int MAX_VOLUME = 100;
+    int MIN_VOLUME = 0;
+    
+    void powerOn();
+    void powerOf();
+    void volumeUp();
+    void volumeDown();
+}
